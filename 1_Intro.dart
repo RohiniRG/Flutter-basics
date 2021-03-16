90d7aea1a6ad4c5665742b7c7f49a9e2d8ccea2e
rohinirg <rohinirao2112@gmail.com>
2021-03-16 19:25:44 +0530

    Intro and data types added

---
+++ 1_Intro.dart
@@ -0,0 +1,30 @@
+// COMMENTS
+
+// Dart is a static type(once you define the type pof variable, you cannot change)
+// programming language
+
+// Dart supports 2 type of compilations:
+// AOT: ahead of time (entire version with some optimization)
+// JIT: just in time (on the fly)
+
+// dart collections or core functionalities are defined in a package
+// dart:core
+// import 'dart:core';
+// imported automatically in every dart program
+
+import 'dart:io';
+
+main() {
+  // no return type in function definition = void
+
+  stdout.writeln('Enter you first name: ');
+  var firstName = stdin.readLineSync(); // var = variable keyword
+  // dart compiler automatically knows this is a string = Type Inference
+
+  stdout.writeln('Enter you last name: ');
+  String? lastName = stdin.readLineSync();
+  // already assigned string, so cannot reassign
+  // String lastName = 2; -> ERROR = Static Type
+
+  print('My name is $firstName $lastName'); // $ = String Interpolation
+}
---
+++ 2_PrimitiveDtypes.dart
@@ -0,0 +1,44 @@
+// Inline comment
+
+/* Block 
+  comment */
+
+/// Documentation
+
+/* Two types of languages:object
+1. Strongly typed: type of variable is known at compile time (C++, Java, Swift)
+2. Dynamically typed: type of variable is known at compile time (Python, Ruby, JS)
+
+BASIC DATA TYPES IN DART: 
+fundamental(int, double, string, bool), dynamic*/
+
+// import 'dart:io';
+
+main() {
+  int amount1 = 100;
+  var amount2 = 200;
+  print('Amount 1: $amount1 | Amount 2: $amount2 \n');
+
+  double damount1 = 10.01;
+  var damount2 = 10.99;
+  print('Amount 1: $damount1 | Amount 2: $damount2 \n');
+
+  String name1 = 'Rohini';
+  var name2 = 'Akhil';
+  print('Name 1: $name1 | Name 2: $name2 \n');
+
+  bool q1 = true;
+  var q2 = false;
+  print('1: $q1 | 2: $q2 \n');
+
+  dynamic weakVar = 100; // dynamic keyword assigns value at runtime
+  print('Weak variable: $weakVar \n');
+
+  weakVar = 'Dart';
+  print('Weak variable: $weakVar \n');
+
+  // DART IS AN OBJECT ORIENTED PROGRAMMING LANGUAGE
+  // Everything is an object including 'null' type
+  weakVar = null;
+  print('Weak variable: $weakVar \n');
+}
